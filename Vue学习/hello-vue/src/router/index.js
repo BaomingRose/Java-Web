0ee30150a10889dc1b30391be067e0e7ebb785dd
BaomingRose <49124801+BaomingRose@users.noreply.github.com>
2021-01-21 08:43:26 +0800

    基于vue简单登录跳转

---
+++ Vue学习/hello-vue/src/router/index.js
@@ -0,0 +1,23 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Login from '../views/Login'
+import Main from '../views/Main'
+
+Vue.use(Router);
+
+export default new Router({
+  routes: [
+    {
+      //登录页
+      path: '/main',
+      name: 'Main',
+      component: Main
+    },
+    {
+      //首页
+      path: '/login',
+      name: 'Login',
+      component: Login
+    },
+  ]
+})
